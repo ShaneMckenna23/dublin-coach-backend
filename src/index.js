78f2f4492a2c96774e3610a780a15d7bbec0d39b
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-19 15:06:54 +0000

    New build tag format

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ import setupStartServer from "./setup/startServer";
 // Create express server
 const server = express();
 
-server.set("port", process.env.PORT || 3000);
+server.set("port", 3000);
 
 server.get("/favicon.ico", function(req, res) {
   res.status(204);
+});
+
+server.get("/", function(req, res) {
+  res.send("Dublin-Coach-Backend");
 });
 
 // Setup load modules
