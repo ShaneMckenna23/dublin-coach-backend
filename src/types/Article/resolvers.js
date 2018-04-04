b72e13247b9e46734907930ddca3963645aafe30
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-04-04 14:33:53 +0100

    New Squidex location

--- src/types/Article/resolvers.js
+++ src/types/Article/resolvers.js
@@ -8,29 +8,11 @@ const resolvers = {
 }
 
 async function getArticles(parentValue, { count }) {
-  let a = {
-    headline: "TEST",
-    extract: "TEST",
-    text: "TEST",
-    link: "TEST",
-    publishDate: "TEST",
-    photo: "TEST",
-    photoDesc: "TEST",
-    author: "TEST"
-  }
-  let mock = []
-  mock.push(a)
-  mock.push(a)
-  mock.push(a)
-  mock.push(a)
-  mock.push(a)
-  return mock;
-
   return await axios
-    .get("https://cloud.squidex.io/api/content/dublin-coach-cms/article?$top="+count+"&$orderby=data/publishDate/iv desc", {
+    .get("https://cloud.squidex.io/api/content/dublin-coach-squidex/article?$top="+count+"&$orderby=data/publishDate/iv desc", {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjkxRkRENEVCRDYwNjMxNURFREI4MENEMDkzMERFRkZBMjFEREE2NkIiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJrZjNVNjlZR01WM3R1QXpRa3czdi1pSGRwbXMifQ.eyJuYmYiOjE1MjEwMzc5NjEsImV4cCI6MTUyMzYyOTk2MSwiaXNzIjoiaHR0cHM6Ly9jbG91ZC5zcXVpZGV4LmlvL2lkZW50aXR5LXNlcnZlciIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnNxdWlkZXguaW8vaWRlbnRpdHktc2VydmVyL3Jlc291cmNlcyIsInNxdWlkZXgtYXBpIl0sImNsaWVudF9pZCI6ImR1Ymxpbi1jb2FjaC1jbXM6ZHVibGluLWNvYWNoLWJhY2tlbmQiLCJzY29wZSI6WyJzcXVpZGV4LWFwaSJdfQ.p828oXPyy0a4no8T_InCqSOpq3GeU1i-H9LaipJI6IFTKO7ElMqKlFg0r67rtYRUvV2v3sBi3WvnvICKtpieBg1iPbkBa8IpX5qHji0eclrgFj2LMCK2gYFmNC-8mtWhO_XW1bv5ppOH9SOPCi9bl7O5GjRphYXallPF7Rf4ArpaE9dr0-2h6qEQ31CH4ZMA6tUYeG9_uBTMS7BKSOgdBJSw7N5mN4oe-iqWWF8AYSGJRB0khdgYna-CbzNYa_FZmh4UpE-AIhFfNZ7Res-O9M5sYlp8sYA9fNxYdlhyAxzxVJfusTTZNS4XhSBriJSNI_C8ILLsL5i_iBKE9FK9Yw"
+          "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjkxRkRENEVCRDYwNjMxNURFREI4MENEMDkzMERFRkZBMjFEREE2NkIiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJrZjNVNjlZR01WM3R1QXpRa3czdi1pSGRwbXMifQ.eyJuYmYiOjE1MjI4NDg0NDgsImV4cCI6MTUyNTQ0MDQ0OCwiaXNzIjoiaHR0cHM6Ly9jbG91ZC5zcXVpZGV4LmlvL2lkZW50aXR5LXNlcnZlciIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnNxdWlkZXguaW8vaWRlbnRpdHktc2VydmVyL3Jlc291cmNlcyIsInNxdWlkZXgtYXBpIl0sImNsaWVudF9pZCI6ImR1Ymxpbi1jb2FjaC1zcXVpZGV4OmZyb250ZW5kIiwic2NvcGUiOlsic3F1aWRleC1hcGkiXX0.m8tGZ5huS_Zj2l2Gpsf1ZzGzy_WAjvO0R7wLL9QOW24vY5gnytKyozCbGCHwb7mOzONn6WmGyq9cgNqpSM5FiW8vsGpS7kAdKIBHLPogNTc-Xo_vgBJqL45AZFEoHz4IFI_szcemsvHKpyOOfJTIt7MuA-drQGCDOWnNkzjZcok60XQRd8vpDHrvg9Qrqm_W5LTTNa-OSrjHksURm79t0cfD4v-ViZJWJK-Dl9bHGRE5vxpXAI__TRgO2luWBZiMlvI_-inJUvoa8qHdg713Epi5hjLDQn8zmgCfcInVpkZrFFfupsWed6-uufPWKX0SpUlChzBfrKwTWfctzS4vEQ"
       }
     })
     .then(function(response) {
